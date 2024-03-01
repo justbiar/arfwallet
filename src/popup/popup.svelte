9a19851f0e9b874d45ee010f1fc101f727ef9f51
Enes Halat <yehalat@outlook.com>
2024-03-01 20:08:35 +0300

    Initial commit

---
+++ src/popup/popup.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  // This is the area where you type your JavaScript code.
+  console.log('ArfWallet popup activated!');
+</script>
+
+<div>
+  <!-- This is the area where you type your HTML code. -->
+  <h1>Welcome to ArfWallet!</h1>
+
+  <p>
+    The wallet app dedicated to the legacy of Cahit Arf.
+  </p>
+</div>
+
+<style>
+  /* This is the area where you type your CSS code. */
+  
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
